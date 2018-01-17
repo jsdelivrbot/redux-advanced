a17c6c6104d1f0d0754e5d5d237b558e7953561b
jklingstedt <jklingstedt@jawbone.com>
2018-01-16 18:44:56 -0800

    adds redux dev tools

--- src/components/app.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class App extends Component { // eslint-disable-line
-    render() { // eslint-disable-line
-        return (
-            <div>React simple starter</div>
-        )
-    }
-}
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 
-import App from './components/app'
+import App from './app'
 import reducers from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore)
+const enhancer = compose(
+    applyMiddleware(),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+)
+const store = createStore(reducers, {}, enhancer)
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>
     , document.querySelector('.container'))
