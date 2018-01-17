a9f79049e1e71d942edeb3820a5ea4c46058375b
jklingstedt <jklingstedt@jawbone.com>
2018-01-16 18:37:46 -0800

    missing files, oops

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "extends": ["jhh-react"]
+};
---
+++ src/components/app.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class App extends Component { // eslint-disable-line
+    render() { // eslint-disable-line
+        return (
+            <div>React simple starter</div>
+        )
+    }
+}
